338e93d19fea17ce89a784c6cacf301810bf14aa
Villaflor, Justin John Mico <152823210+miiciiii@users.noreply.github.com>
2025-01-15 00:01:30 +0800

    Update urls.py

--- ISSC/srvlnc/urls.py
+++ ISSC/srvlnc/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name='home'),
-   path('video_feed/<int:camera_id>/', views.video_feed, name='video_feed'),
-   path('video_feed/check_cams/', views.check_cams, name='check_cams'),
+    path('video_feed/<int:camera_id>/', views.video_feed, name='video_feed'),
+    path('video_feed/check_cams/', views.check_cams, name='check_cams'),
 ]
